5e3792788fdb26e6eb7bd8b56e12b9ffc1e7a199
C4PC10 <C4PC10@C4PC10>
2023-11-15 18:43:42 +0200

    Добавьте файлы проекта.

---
+++ XLSXWork/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace XLSXWork
+{
+    // 1.Открыть Эксэль
+    // 2. Сформировать книгу - лист
+    // 3. Обращение к таблицам
+    // Interop
+    // EPPLUS Library устанавливается в проект
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
