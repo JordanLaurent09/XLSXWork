658297d2ae59f2a4a8f6c99c16af3efa0eab777a
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2023-11-15 19:46:39 +0200

    xlsx basics

--- XLSXWork/Program.cs
+++ XLSXWork/Program.cs
@@ -13,15 +13,44 @@ namespace XLSXWork
     {
         static void Main(string[] args)
         {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
             ExcelPackage newBook = new ExcelPackage("newTable.xlsx");
-            ExcelWorksheet newWorkSheet = newBook.Workbook.Worksheets.Add("Мой лист"); // добавление нового листа в книгу
+            ExcelWorksheet newWorkSheet = newBook.Workbook.Worksheets["Мой лист"];//newBook.Workbook.Worksheets.Add("Мой лист"); // добавление нового листа в книгу
 
-            newWorkSheet.Cells["B5"].Value = "Эта девушка";
-            newWorkSheet.Cells["A7"].Value = "СкубиДу";
-            newWorkSheet.Cells["C3"].Value = "Ячейка да";
-            newWorkSheet.Cells["D2"].Value = "Ячейка нет";
+            string[] arr = {"Ячейка1", "Ячейка2", "Ячейка3", "Ячейка4" };
 
-            newBook.Save();
+            for(int i = 0; i < arr.Length; i++)
+            {
+                newWorkSheet.Cells[i + 1, 1].Value = arr[i];
+            }
+
+            string findingValue = "СкубиДу";
+
+            int row = 0;
+            int column = 0;
+
+            foreach(var cell in newWorkSheet.Cells)
+            {
+                if(cell.Text == findingValue)
+                {
+                    Console.WriteLine("Совпадение найдено");
+
+                    row = cell.Start.Row;
+                    column = cell.Start.Column;
+                }
+            }
+
+            Console.WriteLine($"Значение {findingValue} найдено в ячейке с координатами {row}:{column}");
+
+            // заполнение ячеек
+            //newWorkSheet.Cells["B5"].Value = "Эта девушка";
+            //newWorkSheet.Cells["A7"].Value = "СкубиДу";
+            //newWorkSheet.Cells["C3"].Value = "Ячейка да";
+            //newWorkSheet.Cells["D2"].Value = "Ячейка нет";
+
+            newBook.Save(); // Сохранение книги
+
+            
 
             //ExcelWorksheet existingWorksheet = newBook.Workbook.Worksheets["Мой лист"]; // добавление к существующему листу
 
