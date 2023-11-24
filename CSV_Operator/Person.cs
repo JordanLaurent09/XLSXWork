902055d69f99b5c694d517ff5e82486aa06033c5
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2023-11-24 19:00:19 +0200

    csv work

---
+++ CSV_Operator/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace CSV_Operator
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+    }
+}
---
+++ CSV_Operator/Program.cs
@@ -0,0 +1,26 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+namespace CSV_Operator
+{
+    // library CSVhelper
+    /// <summary>
+    /// Запись объектов в CSV-файл - используется объект класса StreamWriter
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter writer = new StreamWriter("example.csv");
+
+            CsvWriter csvWriter = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture));
+
+            List<Person> people = new List<Person>()
+            {
+                new Person(){Name = "Albert", Age = 24},
+                new Person(){Name = "Dmitry", Age = 36},
+                new Person(){Name = "Aleksey", Age = 18}
+            };
+        }
+    }
+}
