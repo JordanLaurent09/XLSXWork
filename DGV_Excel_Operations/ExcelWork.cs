c2497515a20e9e2778c1c634d36588c3a97fa9b7
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2023-11-24 18:28:48 +0200

    excel practice

--- DGV_Excel_Operations/ExcelWork.cs
+++ DGV_Excel_Operations/ExcelWork.cs
@@ -16,19 +16,19 @@ namespace DGV_Excel_Operations
 
             for(int i = 0; i < studentsSheet.Workbook.Worksheets.Count; i++)
             {
-                string studentCode = studentsSheet.Cells[1, i + 1].Text;
+                string studentCode = studentsSheet.Cells[i + 1, 1].Text;
 
-                string? studentName = studentsSheet.Cells[2, i + 1].Text;
+                string? studentName = studentsSheet.Cells[i + 1, 2].Text;
 
-                string? studentSurname = studentsSheet.Cells[3, i + 1].Text;
+                string? studentSurname = studentsSheet.Cells[i + 1, 3].Text;
 
-                string studentAge = studentsSheet.Cells[4, i + 1].Text;
+                string studentAge = studentsSheet.Cells[i + 1, 4].Text;
 
-                string? studentFaculty = studentsSheet.Cells[5, i + 1].Text;
+                string? studentFaculty = studentsSheet.Cells[i + 1, 5].Text;
 
-                string studentCourse = studentsSheet.Cells[6, i + 1].Text;
+                string studentCourse = studentsSheet.Cells[i + 1, 6].Text;
 
-                string? studentGroup = studentsSheet.Cells[7, i + 1].Text;
+                string? studentGroup = studentsSheet.Cells[i + 1, 7].Text;
 
                 students.Add(new Student(studentName, studentSurname, studentAge, studentFaculty, studentCourse, studentGroup, studentCode));
             }
