3b0381a526c0d96abf958dcf54ca8ea79af664af
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2023-11-24 19:15:38 +0200

    csv work

--- CSV_Operator/Program.cs
+++ CSV_Operator/Program.cs
@@ -6,21 +6,40 @@ namespace CSV_Operator
     // library CSVhelper
     /// <summary>
     /// Запись объектов в CSV-файл - используется объект класса StreamWriter
+    /// Чтение объектов из CSV-файла - испольуется объект класса StreamReader
     /// </summary>
     internal class Program
     {
         static void Main(string[] args)
         {
-            StreamWriter writer = new StreamWriter("example.csv");
+            // Чтение из файла
+            StreamReader reader = new StreamReader("example.csv");
 
-            CsvWriter csvWriter = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture));
+            CsvReader csvReader = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture));
 
-            List<Person> people = new List<Person>()
+            List<Person> people = csvReader.GetRecords<Person>().ToList();
+
+            reader.Close();
+
+            foreach(Person person in people)
             {
-                new Person(){Name = "Albert", Age = 24},
-                new Person(){Name = "Dmitry", Age = 36},
-                new Person(){Name = "Aleksey", Age = 18}
-            };
+                Console.WriteLine($"Имя - {person.Name}, возраст - {person.Age}");
+            }
+
+            // Запись в файл
+            //StreamWriter writer = new StreamWriter("example.csv");
+
+            //CsvWriter csvWriter = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture));
+
+            //List<Person> people = new List<Person>()
+            //{
+            //    new Person(){Name = "Albert", Age = 24},
+            //    new Person(){Name = "Dmitry", Age = 36},
+            //    new Person(){Name = "Aleksey", Age = 18}
+            //};
+
+            //csvWriter.WriteRecords(people);
+            //writer.Close();
         }
     }
 }
