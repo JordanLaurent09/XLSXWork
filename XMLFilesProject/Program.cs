a0ce57fdca095fbe2437d180942ab978f3820e06
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2023-11-29 21:21:37 +0200

    XML basics

--- XMLFilesProject/Program.cs
+++ XMLFilesProject/Program.cs
@@ -3,6 +3,7 @@ namespace XMLFilesProject
 {
     class Program
     {
+        private static List<Goods>? _listOfGoods = new List<Goods>();
         static void Main(string[] args)
         {
             //List<Person> people = new List<Person>() {
@@ -18,15 +19,63 @@ namespace XMLFilesProject
 
             //stream.Close();
 
-            XmlSerializer deserialize = new XmlSerializer(typeof(List<Person>));
-            List<Person> list;
-            using (FileStream deserializeStream = new FileStream("persons.xml", FileMode.Open))
-            {
-                list = (List<Person>)deserialize.Deserialize(deserializeStream)!;
+            //XmlSerializer deserialize = new XmlSerializer(typeof(List<Person>));
+            //List<Person> list;
+            //using (FileStream deserializeStream = new FileStream("persons.xml", FileMode.Open))
+            //{
+            //    list = (List<Person>)deserialize.Deserialize(deserializeStream)!;
 
-                foreach(var person in list)
+            //    foreach(var person in list)
+            //    {
+            //        Console.WriteLine($"{person.Name} {person.Age}");
+            //    }
+            //}
+
+            List<Goods> goods = new List<Goods>()
+            {
+                new Goods(){ Title = "Блинчики", Description = "Очень вкусные блинчики с сыром и ветчиной", Price = 23.22},
+                new Goods(){ Title = "Пончики", Description = "Очень вкусные пончики в пудре", Price = 77.13},
+                new Goods(){ Title = "Кофе", Description = "Очень вкусное кофе", Price = 50.27}
+            };
+
+            //XmlMethods.SerializeGoods(goods);
+            //List<Goods> newGoods = XmlMethods.DeserializeGoods();
+            
+            
+            //foreach(var good in newGoods)
+            //{
+            //    Console.WriteLine($"{good.Title} {good.Description} {good.Price}");
+            //}
+
+            bool isExit = false;
+
+            while(true)
+            {
+                Console.WriteLine("Что хотите сделать?\nПРОСМОТР - Просмотреть список товаров\nДОБАВИТЬ - добавить товар в список");
+                string? option = Console.ReadLine();
+                switch (option)
                 {
-                    Console.WriteLine($"{person.Name} {person.Age}");
+                    case "ПРОСМОТР":
+                        _listOfGoods = XmlMethods.DeserializeGoods();
+
+
+                        foreach (var good in _listOfGoods)
+                        {
+                            Console.WriteLine($"{good.Title} {good.Description} {good.Price}");
+                        }
+                        break;
+
+                    case "ДОБАВИТЬ":
+                        Console.WriteLine("Введите название товара");
+                        string? title = Console.ReadLine();
+                        Console.WriteLine("Введите описание товара");
+                        string? description = Console.ReadLine();
+                        Console.WriteLine("Введите цену товара");
+                        double price = double.Parse(Console.ReadLine()!);
+                        Goods newGood = new Goods() { Title = title!, Description = description!, Price = price!};
+                        _listOfGoods.Add(newGood);
+                        XmlMethods.SerializeGoods(_listOfGoods);
+                        break;
                 }
             }
         }
---
+++ XMLFilesProject/XmlMethods.cs
@@ -0,0 +1,35 @@
+﻿using System.Xml.Serialization;
+
+namespace XMLFilesProject
+{
+    static class XmlMethods  // Класс с методами для сериализации/десериализации класса товаров
+    {
+        // Метод для сериализации списка товаров
+       
+        public static void SerializeGoods(List<Goods> goods)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Goods>));
+
+            FileStream stream = new FileStream("goods.xml", FileMode.OpenOrCreate);
+            serializer.Serialize(stream, goods);
+
+            stream.Close();
+        }
+
+        // Метод для десериализации списка товаров
+
+        public static List<Goods> DeserializeGoods()
+        {
+            XmlSerializer deserialize = new XmlSerializer(typeof(List<Goods>));
+            List<Goods> list;
+            using (FileStream deserializeStream = new FileStream("goods.xml", FileMode.Open))
+            {
+                list = (List<Goods>)deserialize.Deserialize(deserializeStream)!;
+
+                
+            }
+
+            return list;
+        }
+    }
+}
