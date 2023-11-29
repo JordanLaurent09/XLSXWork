677307488fae78c2c820bcc22b24e99f4d860db8
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2023-11-29 20:49:45 +0200

    xml basics

---
+++ XMLFilesProject/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace XMLFilesProject
+{
+    [Serializable] //  Указывает, что данный класс может быть сериализованным
+    public class Person
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ XMLFilesProject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Xml.Serialization; // классы для работы с xml с целью сериализации/десериализации
+namespace XMLFilesProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<Person> people = new List<Person>() {
+            //new Person() { Name = "Валерий", Age = 56 },
+            //new Person() { Name = "Николай", Age = 22 },
+            //new Person() { Name = "Владислав", Age = 16 }
+            //};
+
+            //XmlSerializer serializer = new XmlSerializer(typeof (List<Person>));
+
+            //FileStream stream = new FileStream("persons.xml", FileMode.Create);
+            //serializer.Serialize(stream, people);
+
+            //stream.Close();
+
+            XmlSerializer deserialize = new XmlSerializer(typeof(List<Person>));
+            List<Person> list;
+            using (FileStream deserializeStream = new FileStream("persons.xml", FileMode.Open))
+            {
+                list = (List<Person>)deserialize.Deserialize(deserializeStream)!;
+
+                foreach(var person in list)
+                {
+                    Console.WriteLine($"{person.Name} {person.Age}");
+                }
+            }
+        }
+    }
+}
